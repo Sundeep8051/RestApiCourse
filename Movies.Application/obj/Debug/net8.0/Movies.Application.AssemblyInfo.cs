b5ecb8b9064e2505a874c3072b8b63f8154fb09b
Sundeep Manibhushan <sundeep.manibhushan@gmail.com>
2024-11-30 14:55:57 +0530

    ts

--- Movies.Application/obj/Debug/net8.0/Movies.Application.AssemblyInfo.cs
+++ Movies.Application/obj/Debug/net8.0/Movies.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Movies.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04a1fb80c4f599c5e5b78bf3784ecf39df777c95")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+945f50fb20fb2730c44c0ae10a8b9b6c4dd25f53")]
 [assembly: System.Reflection.AssemblyProductAttribute("Movies.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Movies.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
